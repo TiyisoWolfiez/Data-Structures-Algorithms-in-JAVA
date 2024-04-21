bb6886b06372eb598436a975c1a1f8edfb030b50
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-04-21 10:10:10 +0200

    Update BinaryNode.java
    completion

--- Codes/BST(Binary Search Tree)/BinaryNode.java
+++ Codes/BST(Binary Search Tree)/BinaryNode.java
@@ -10,3 +10,18 @@ public class BinaryNode<T extends Comparable<T>> {
         this.left = null;
         this.right = null;
     }
+    public BinaryNode(T data, BinaryNode<T> left, BinaryNode<T> right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public String toString() {
+        String leftString = (left == null) ? "N" : left.data.toString();
+        String rightString = (right == null) ? "N" : right.data.toString();
+        String out = "[" + leftString + "]<-[" + data.toString() + "]->[" + rightString + "]";
+
+        return out;
+    }
+
+}
