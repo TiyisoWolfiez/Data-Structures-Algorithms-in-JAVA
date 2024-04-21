d736c7c9783779d2691f6ff641ddf56aa735b430
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-04-21 10:09:40 +0200

    Update BinaryNode.java
    Binary Node implementation

--- Codes/BST(Binary Search Tree)/BinaryNode.java
+++ Codes/BST(Binary Search Tree)/BinaryNode.java
@@ -1,1 +1,12 @@
+public class BinaryNode<T extends Comparable<T>> {
 
+    public T data;
+
+    public BinaryNode<T> left;
+    public BinaryNode<T> right;
+
+    public BinaryNode(T data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
