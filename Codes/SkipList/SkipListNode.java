a88d2cbad8859528514079fa6bf15a8536a09f9c
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-04-23 19:04:42 +0200

    Update SkipListNode.java
    Implementation of SkipListNode.java

--- Codes/SkipList/SkipListNode.java
+++ Codes/SkipList/SkipListNode.java
@@ -1,1 +1,28 @@
+// This is used to remove the warnings which occur when using generics
+@SuppressWarnings("unchecked")
+public class SkipListNode<T extends Comparable<T>> {
+    public T key;
+    public SkipListNode<T>[] next;
 
+    public SkipListNode(T key, int levels) {
+        this.key = key;
+        this.next = new SkipListNode[levels];
+        for(int k = 0; k < levels ; k++){
+            next[k] = null;
+        } 
+    }
+
+    @Override
+    public String toString() {
+        return "["+ this.key.toString() + "]";
+    }
+
+    public String emptyString() {
+        int k = toString().length();
+        String p = "";
+        for (int l = 0; l < k; l++) {
+            p += "-";
+        }
+        return p;
+    }
+}
