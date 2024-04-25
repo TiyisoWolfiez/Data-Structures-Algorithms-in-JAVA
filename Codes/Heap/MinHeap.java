9e8d2ef30b213d220b6318786717af5581a12c23
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-04-25 20:15:14 +0200

    Update MinHeap.java

--- Codes/Heap/MinHeap.java
+++ Codes/Heap/MinHeap.java
@@ -1,1 +1,16 @@
+class MinHeap<T extends Comparable<T>> extends Heap<T> {
 
+    public MinHeap() {
+        super();
+    }
+
+    public MinHeap(T[] array) {
+        super(array);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected boolean compare(Comparable<T> child, Comparable<T> parent) {
+        return child.compareTo((T) parent) < 0;
+    }
+}
