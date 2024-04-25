1cc8fd830d5d14f060c5d44ef762ade9144b79d3
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-04-25 20:15:40 +0200

    Update MaxHeap.java

--- Codes/Heap/MaxHeap.java
+++ Codes/Heap/MaxHeap.java
@@ -1,1 +1,15 @@
+class MaxHeap<T extends Comparable<T>> extends Heap<T> {
 
+    public MaxHeap() {
+        super();
+    }
+
+    public MaxHeap(T[] array) {
+        super(array);
+    }
+
+    @SuppressWarnings("unchecked")
+    protected boolean compare(Comparable<T> child, Comparable<T> parent) {
+        return child.compareTo((T) parent) > 0;
+    }
+}
