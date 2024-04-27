d4c173ea4cf92a43808483c4881e8ac1b9bba6f7
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-04-27 11:23:31 +0200

    Update Extraction.java

--- Codes/HashFunction/Extraction.java
+++ Codes/HashFunction/Extraction.java
@@ -1,1 +1,20 @@
+public class Extraction extends HashFunction {
+    public Extraction(int n) {
+        num = n;
+    }
 
+    @Override
+    public int hash(String input) {
+        String numString = stringToNum(input);
+        int size = numString.length();
+        if (size > num) {
+            String last = "";
+            for (int i = size - 1, j = 0; j < num; i--, j++) {
+                last = numString.charAt(i) + last;
+            }
+            numString = last;
+        }
+        return Integer.parseInt(numString);
+    }
+
+}
