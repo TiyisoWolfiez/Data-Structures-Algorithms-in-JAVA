1ae61fd5addf5459a1da3a16e0561099db07428e
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-04-27 11:25:56 +0200

    Update Division.java

--- Codes/HashFunction/Division.java
+++ Codes/HashFunction/Division.java
@@ -1,1 +1,14 @@
+public class Division extends HashFunction {
 
+    public Division(int n) {
+        num = n;
+    }
+
+    @Override
+    public int hash(String input) {
+        String numString = stringToNum(input);
+        long number = Long.parseLong(numString);
+        long finalv = number % num;
+        return (int) finalv;
+    }
+}
