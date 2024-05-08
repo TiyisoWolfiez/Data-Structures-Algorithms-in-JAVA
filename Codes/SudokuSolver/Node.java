655e2f75ea179709b2fb1ed62d7fec1c3faccfba
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-08 20:03:34 +0200

    Update Node.java
    Implementation of Node.java

--- Codes/SudokuSolver/Node.java
+++ Codes/SudokuSolver/Node.java
@@ -1,1 +1,13 @@
+public class Node<T> {
+    public T data;
+    public Node<T> next;
 
+    public String toString() {
+        return data.toString();
+    }
+
+    public Node(T val) {
+        next = null;
+        data = val;
+    }
+}
