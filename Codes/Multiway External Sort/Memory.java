22a353503d8ff8ec3cdc458f186d39f580520cfb
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-10 10:50:22 +0200

    Update Memory.java

--- Codes/Multiway External Sort/Memory.java
+++ Codes/Multiway External Sort/Memory.java
@@ -1,1 +1,31 @@
+class Memory {
+    private Integer[] data;
+    private int size;
 
+    public Memory(int size) {
+        this.data = new Integer[size];
+        this.size = size;
+    }
+
+    public Integer get(int index) {
+        return data[index];
+    }
+
+    public void set(int index, int value) {
+        data[index] = value;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Integer[] getData() {
+        return data;
+    }
+
+    public void clear() {
+        for (int i = 0; i < data.length; i++) {
+            data[i] = null;
+        }
+    }
+}
