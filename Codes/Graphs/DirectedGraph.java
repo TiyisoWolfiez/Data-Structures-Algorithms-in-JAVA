364b3a1d7dac147be2c1fe936765bb959b03ec33
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-21 12:26:21 +0200

    Update DirectedGraph.java

--- Codes/Graphs/DirectedGraph.java
+++ Codes/Graphs/DirectedGraph.java
@@ -1,1 +1,18 @@
 
+public class DirectedGraph<T> extends Graph<T> {
+
+    public DirectedGraph(GraphImplementation<T> impl) {
+        super(impl);
+    }
+
+    @Override
+    public void addEdge(T from, T to) {
+
+    }
+
+    @Override
+    public void removeEdge(T from, T to) {
+
+    }
+
+}
