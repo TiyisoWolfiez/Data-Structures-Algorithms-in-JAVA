7fa54568c7e7f141cc8fec9a6d1f93b07ed099ab
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-21 12:25:46 +0200

    Update Graph.java

--- Codes/Graphs/Graph.java
+++ Codes/Graphs/Graph.java
@@ -1,1 +1,35 @@
+public abstract class Graph<T> {
+    public GraphImplementation<T> impl;
 
+    Graph(GraphImplementation<T> impl) {
+        this.impl = impl;
+    }
+
+    public void addNode(T node) {
+
+    }
+
+    abstract void addEdge(T from, T to);
+
+    public void removeNode(T node) {
+
+    }
+
+    abstract void removeEdge(T from, T to);
+
+    public void display() {
+
+    }
+
+    public LinkedList<T> bfs(T startNode) {
+
+    }
+
+    public LinkedList<T> dfs(T startNode) {
+
+    }
+
+    public LinkedList<T> unweightedShortestPath(T startNode, T endNode) {
+
+    }
+}
