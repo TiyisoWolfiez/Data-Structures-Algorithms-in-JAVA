2fa7b5f2f9ef541f9f2b24c5adebe4eb6149932a
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-23 19:11:03 +0200

    Update UndirectedGraph.java
    Implementation of UndirectedGraph.java

--- Codes/Graphs/UndirectedGraph.java
+++ Codes/Graphs/UndirectedGraph.java
@@ -7,11 +7,11 @@ public class UndirectedGraph<T> extends Graph<T> {
 
     @Override
     public void addEdge(T from, T to) {
-
+        impl.addEdge(from, to);
     }
 
     @Override
     public void removeEdge(T from, T to) {
-
+        impl.removeEdge(from, to);
     }
 }
