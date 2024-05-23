317255d53eef8ee43fc90ff6c6cd6c5f235406e9
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-23 19:11:41 +0200

    Update DirectedGraph.java
    Implementation of DirectedGraph.java

--- Codes/Graphs/DirectedGraph.java
+++ Codes/Graphs/DirectedGraph.java
@@ -7,12 +7,18 @@ public class DirectedGraph<T> extends Graph<T> {
 
     @Override
     public void addEdge(T from, T to) {
-
+        // Add edge from 'from' to 'to'
+        impl.addEdge(from, to);
+        // Since it's an undirected graph, add edge from 'to' to 'from' as well
+        impl.addEdge(to, from);
     }
 
     @Override
     public void removeEdge(T from, T to) {
-
+        // Remove edge from 'from' to 'to'
+        impl.removeEdge(from, to);
+        // Also, remove edge from 'to' to 'from'
+        impl.removeEdge(to, from);
     }
 
 }
